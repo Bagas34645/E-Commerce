81fd768ec824c367b1ba55d780446e9eaa2eda24
Bagas34645 <bagasabiyu1234@gmail.com>
2025-07-11 18:31:39 +0700

    Ganti Gambar & Judul

--- home.php
+++ home.php
@@ -20,7 +20,7 @@ include 'components/add_cart.php';
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>home</title>
+   <title>Home</title>
 
    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
 
@@ -50,7 +50,7 @@ include 'components/add_cart.php';
                <a href="menu.php" class="btn">see menus</a>
             </div>
             <div class="image">
-               <img src="images/home-img-1.png" alt="">
+               <img src="images/home-img-1.jpg" alt="">
             </div>
          </div>
 
